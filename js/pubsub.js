72694ba78191484c0e3e3ded819b72392761675d
tsvetislav <tsvetislavt99@gmail.com>
2021-09-18 11:30:57 +0300

    basic pub-sub module

---
+++ js/pubsub.js
@@ -0,0 +1,30 @@
+/**********
+ *
+ * pubsub.subscribe() on() add() listen()
+ * pubsub.unsubscribe() off() remove() unlisten()
+ * pubsub.publish() emit() announce()
+ *
+ * */
+
+export const pubsub = {
+  events: {},
+  subscribe: function (evName, fn) {
+    console.info(`PS: Someone subscribed to ${evName}`);
+    this.events[evName] = this.events[evName] || [];
+    this.events[evName].push(fn);
+  },
+  unsubscribe: function (evName, fn) {
+    console.info(`PS: Someone unsubscribed from ${evName}`);
+    if (this.events[evName]) {
+      this.events[evName].filter((f) => f !== fn);
+    }
+  },
+  publish: function (evName, data) {
+    console.log(`PS: Making a broadcast about ${evName} with ${data}`);
+    if (this.events[evName]) {
+      this.events[evName].forEach((f) => {
+        f(data);
+      });
+    }
+  },
+};
